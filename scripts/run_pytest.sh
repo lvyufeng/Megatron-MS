02efcac9fba65c0d047a6808440921431b42f840
lvyufeng <lvyufeng2007@hotmail.com>
2024-12-16 10:25:01 +0800

    support dataset

--- scripts/run_pytest.sh
+++ scripts/run_pytest.sh
@@ -1,1 +1,3 @@
 msrun --worker_num=8 --local_worker_num=8 --master_port=8118 --log_dir=msrun_log --join=True --cluster_time_out=300 pytest -vs tests/unit_tests/dist_checkpointing/test_optimizer.py::TestDistributedOptimizer
+
+mpirun -n 8 pytest -vs tests/unit_tests/dist_checkpointing/test_optimizer.py::TestDistributedOptimizer
